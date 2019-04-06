95151f3509311e14113c7e1c1683c4278b294f28
KJ <abc8339265@gmail.com>
2019-04-06 05:50:24 +0000

    change incoding system EUC-KR -> UTF-8

--- src/Slime.cpp
+++ src/Slime.cpp
@@ -1,1 +1,26 @@
+#include "Slime.h"
 
+Slime::Slime()
+{
+	
+	// set status
+	Unit::set_unit_type(g_SLIME);
+	Unit::set_STR(5);
+	Unit::set_INT(0);
+	Unit::set_DEX(5);
+	Unit::set_CON(6);
+	Unit::set_LUK(5);
+
+	Monster::set_giveExp(20);
+		
+	Unit::set_status();
+		
+			 
+	std::cout << "Slime : " <<  Unit::get_pos() << std::endl;
+
+	Monster::set_monster_position();
+
+	Point::gotoxy(get_pos());
+	Unit::draw_something(Unit::get_pos(), Unit::get_unit_type());
+			
+}
